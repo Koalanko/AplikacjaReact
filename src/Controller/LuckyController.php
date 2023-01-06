302e7969451b9b6f7ccf7d8f73ae3ee0ac9ab456
Koalanko <przybyskrzysztof@gmail.com>
2023-01-06 14:46:09 +0100

    P

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,15 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-
-class LuckyController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class LuckyController extends AbstractController
 {
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
