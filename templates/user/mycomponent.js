1b285a54b4e8775f31f923cc25f88290126bad2e
Koalanko <przybyskrzysztof@gmail.com>
2023-01-08 19:59:55 +0100

    P

--- templates/user/mycomponent.js
+++ templates/user/mycomponent.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 class MyComponent extends React.Component {
     render() {
-        return <h1>Hello, World!</h1>;
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+            </div>
+        );
     }
 }
+
+export default MyComponent;
