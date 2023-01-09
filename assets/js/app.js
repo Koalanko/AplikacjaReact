496740aea7197a3527e95d5daefb078fb867c01a
Koalanko <przybyskrzysztof@gmail.com>
2023-01-09 17:54:16 +0100

    P

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, World!</h1>;
-const element2 = <p1>Hello, World!</p1>;
+const element = <h1>Hello, World111111!</h1>;
+const element2 = <p1>Hello, World111111!</p1>;
 ReactDOM.render(element2, document.getElementById('root'));
