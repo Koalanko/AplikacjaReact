5a59afed6e7b7f1417cd1ddc607662db98535f8f
Koalanko <przybyskrzysztof@gmail.com>
2023-01-09 18:33:46 +0100

    P

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,6 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-
-// start the Stimulus application
-import './bootstrap';
-import { registerReactControllerComponents } from '@symfony/ux-react';
-
-// Registers React controller components to allow loading them from Twig
-//
-// React controller components are components that are meant to be rendered
-// from Twig. These component then rely on other components that won't be called
-// directly from Twig.
-//
-// By putting only controller components in `react/controllers`, you ensure that
-// internal components won't be automatically included in your JS built file if
-// they are not necessary.
-registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
+const element = <h1>Hello, World1!</h1>;
+const element2 = <p1>Hello, World111111!</p1>;
+ReactDOM.render(element2, document.getElementById('app'));
